a1ee597f592d71b5e649fe8a6583f6d07d201087
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2025-08-22 14:23:05 +0000

    Initial commit

---
+++ src/pages/Ajustes de la cuenta.gbge3.js
@@ -0,0 +1,24 @@
+// Referencia de la API de Velo: https://www.wix.com/velo/reference/api-overview/introduction
+import (fetch) from 'wix-fetch';
+$w.onReady(function () {
+
+	// Escribe tu código de Javascript aquí usando la API de marco de Velo 
+
+	// Escribe hola mundo:
+	// console.log("¡Hola mundo!");
+
+	// Llama las funciones en los elementos de la página, por ejemplo:
+	// $w("#button1").label = "¡Haz clic aquí!";
+
+	// Haz clic en "Ejecutar", o ve a la vista previa de tu sitio, para ejecutar el código
+
+});
+
+function getDataApi(params) {
+	const url = https://randomuser.me/api/?page=1&results=3;
+	fetch(url, 
+	"Method": "Get",
+	"Headers": {"
+	"Content-type": 'aplicattion/json'
+	};
+}
---
+++ src/pages/Inicio.c1dmp.js
@@ -0,0 +1,8 @@
+// Referencia de la API de Velo: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+console.log("Página cargada");
+
+});
+
